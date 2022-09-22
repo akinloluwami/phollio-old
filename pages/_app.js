9f29dd422096753469832bf0cecb1335651455df
Akinkunmi <akinkunmiisrael41@gmail.com>
2022-09-22 05:43:00 +0100

    .

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,60 @@
-import '../styles/globals.css'
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+const variantOutlined = () => ({
+  field: {
+    _focus: {
+      borderColor: "var(--chakra-ui-focus-ring-color)",
+      boxShadow: "0 0 0 1px var(--chakra-ui-focus-ring-color)",
+    },
+  },
+});
 
-export default MyApp
+const theme = extendTheme({
+  fonts: {
+    body: "Inter, sans-serif",
+    heading: "Inter, sans-serif",
+  },
+  colors: {
+    brand: {
+      accent: "#F66C28",
+      accentDark: "#fa661d",
+    },
+  },
+  styles: {
+    global: {
+      ":host,:root": {
+        "--chakra-ui-focus-ring-color": "#F66C28",
+      },
+    },
+  },
+  shadows: {
+    outline: "0 0 0 1px var(--chakra-ui-focus-ring-color)",
+  },
+  components: {
+    Input: {
+      variants: {
+        outline: variantOutlined,
+      },
+    },
+    Select: {
+      variants: {
+        outline: variantOutlined,
+      },
+    },
+    Textarea: {
+      variants: {
+        outline: () => variantOutlined().field,
+      },
+    },
+  },
+});
+
+const App = ({ Component, pageProps }) => {
+  return (
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
+};
+
+export default App;
