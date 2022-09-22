697b9141266fd80b0764ded5f7f7c1041a100716
Akinkunmi <akinkunmiisrael41@gmail.com>
2022-09-22 05:44:19 +0100

    .

---
+++ pages/landing.jsx
@@ -0,0 +1,13 @@
+import { Box } from "@chakra-ui/react";
+import { useState } from "react";
+import Navbar from "../components/Navbar";
+
+const landing = () => {
+  return (
+    <Box>
+      <Navbar />
+    </Box>
+  );
+};
+
+export default landing;
