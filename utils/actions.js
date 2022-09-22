c44b89e3e7961dc0f230d49b53a9d0ad1e7cdaf4
Akinkunmi <akinkunmiisrael41@gmail.com>
2022-09-22 05:45:33 +0100

    created actions helper for different login and sign up strategies

---
+++ utils/actions.js
@@ -0,0 +1,14 @@
+const withGitHub = () => {
+  console.log("GitHub");
+};
+const withTwitter = () => {
+  console.log("Twitter");
+};
+const withApple = () => {
+  console.log("Apple");
+};
+const withGoogle = () => {
+  console.log("Google");
+};
+
+export { withApple, withGitHub, withGoogle, withTwitter };
