f4998940ba5009e1a30ef2ae8f4f12aa327479d3
Akinkunmi <akinkunmiisrael41@gmail.com>
2022-09-23 14:43:49 +0100

    wrapped in dashbaord layout

---
+++ pages/dashboard/profile.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import DashboardLayout from "../../layouts/DashboardLayout";
+
+const Profile = () => {
+  return <DashboardLayout>Profile</DashboardLayout>;
+};
+
+export default Profile;
