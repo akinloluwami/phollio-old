46a4106ca5882d1f7c556bbbf872956903704ba1
Akinkunmi <akinkunmiisrael41@gmail.com>
2022-09-23 14:42:04 +0100

    wrapped in the dashboard layout

---
+++ pages/dashboard/analytics.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import DashboardLayout from "../../layouts/DashboardLayout";
+
+const Aanalytics = () => {
+  return <DashboardLayout>Aanalytics</DashboardLayout>;
+};
+
+export default Aanalytics;
