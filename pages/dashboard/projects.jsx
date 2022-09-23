bf0adf33eeeea8b6b4540b54319880629a9495bc
Akinkunmi <akinkunmiisrael41@gmail.com>
2022-09-23 14:44:00 +0100

    wrapped in dashbaord layout

---
+++ pages/dashboard/projects.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import DashboardLayout from "../../layouts/DashboardLayout";
+
+const Projects = () => {
+  return <DashboardLayout>Projects</DashboardLayout>;
+};
+
+export default Projects;
