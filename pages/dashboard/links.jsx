9870f567e4bfb72577a1b201f9e8d7ea366f0a32
Akinkunmi <akinkunmiisrael41@gmail.com>
2022-09-23 14:43:26 +0100

    renamed extension to jsx

---
+++ pages/dashboard/links.jsx
@@ -0,0 +1,13 @@
+import { Box } from "@chakra-ui/react";
+import React from "react";
+import DashboardLayout from "../../layouts/DashboardLayout";
+
+const Links = () => {
+  return (
+    <DashboardLayout>
+      <Box>Hiiii</Box>
+    </DashboardLayout>
+  );
+};
+
+export default Links;
