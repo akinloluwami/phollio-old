b9c7a63fe4096589ab9409fcb96b7548a3e49749
Akinkunmi <akinkunmiisrael41@gmail.com>
2022-09-23 14:45:00 +0100

    .

---
+++ utils/navigationLinks.js
@@ -0,0 +1,24 @@
+const Links = [
+  {
+    route: "/links",
+    title: "Links",
+  },
+  {
+    route: "/projects",
+    title: "Projects",
+  },
+  {
+    route: "/profile",
+    title: "Profile",
+  },
+  {
+    route: "/appearance",
+    title: "Appearance",
+  },
+  {
+    route: "/analytics",
+    title: "Analytics",
+  },
+];
+
+export default Links;
