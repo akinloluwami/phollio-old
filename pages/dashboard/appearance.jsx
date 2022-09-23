c2cb84fcc3084adfbb32b1e1441c74d20086a358
Akinkunmi <akinkunmiisrael41@gmail.com>
2022-09-23 14:42:20 +0100

    wrapped in dashboard layout

---
+++ pages/dashboard/appearance.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import DashboardLayout from "../../layouts/DashboardLayout";
+
+const Appearance = () => {
+  return <DashboardLayout>Appearance</DashboardLayout>;
+};
+
+export default Appearance;
