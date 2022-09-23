dd10a5504b4fa7cec6297c275bb81c8c3c389dcd
Akinkunmi <akinkunmiisrael41@gmail.com>
2022-09-23 13:51:11 +0100

    added tiny sidebar

--- components/dashboard/Sidebar.jsx
+++ components/dashboard/Sidebar.jsx
@@ -1,7 +1,23 @@
+import { Avatar, Box, Flex } from "@chakra-ui/react";
 import React from "react";
-
+import Logo from "../../components/Logo";
+import Profile from "./Profile";
 const Sidebar = () => {
-  return <div>Sidebar</div>;
+  return (
+    <Flex
+      boxShadow={"base"}
+      height={"100vh"}
+      py={2}
+      flexDirection={"column"}
+      alignItems={"center"}
+      justifyContent={"space-between"}
+    >
+      <Logo />
+      <Box>
+        <Profile />
+      </Box>
+    </Flex>
+  );
 };
 
 export default Sidebar;
