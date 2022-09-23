6b51558158a51b92d5b5f897057949e7b54bfbcf
Akinkunmi <akinkunmiisrael41@gmail.com>
2022-09-23 14:39:11 +0100

    added link bar

---
+++ components/dashboard/Linkbar.jsx
@@ -0,0 +1,31 @@
+import { Box, Button, Flex, Link, Text } from "@chakra-ui/react";
+import React from "react";
+
+const Linkbar = () => {
+  return (
+    <Flex
+      boxShadow="base"
+      px={6}
+      py={2}
+      gap={5}
+      alignItems={"center"}
+      justifyContent={"space-between"}
+    >
+      <Flex>
+        <Text>My Phollio:</Text>
+        <Link href="https://phollio.com/akinkunmi" ml={2} target={"_blank"}>
+          <Text
+            fontSize={"sm"}
+            textDecoration="underline"
+            color={"brand.accent"}
+          >
+            https://phollio.com/akinknumi
+          </Text>
+        </Link>
+      </Flex>
+      <Button backgroundColor={"brand.accent"}>Share</Button>
+    </Flex>
+  );
+};
+
+export default Linkbar;
