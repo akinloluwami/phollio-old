4f646fd0eba1145689a4d312ea1e54b5c3a669ff
Akinkunmi <akinkunmiisrael41@gmail.com>
2022-09-23 14:40:01 +0100

    added navigation

--- layouts/DashboardLayout.jsx
+++ layouts/DashboardLayout.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { Box, Flex } from "@chakra-ui/react";
 import Sidebar from "../components/dashboard/Sidebar";
+import Navigation from "../components/dashboard/Navigation";
+import Linkbar from "../components/dashboard/Linkbar";
 
 const DashboardLayout = ({ children }) => {
   return (
@@ -8,12 +10,12 @@ const DashboardLayout = ({ children }) => {
       <Box>
         <Sidebar />
       </Box>
-      <Box>
-        <Flex>Navbar</Flex>
+      <Box w={"50%"} boxShadow={"base"}>
+        <Navigation />
         <Box>{children}</Box>
       </Box>
-      <Box>
-        <Flex>Link Bar</Flex>
+      <Box w={"50%"} boxShadow={"base"}>
+        <Linkbar />
         <Box>Page Preview</Box>
       </Box>
     </Flex>
