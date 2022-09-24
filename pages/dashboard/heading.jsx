4c140ce5f39d9a19fd5dbd1ac9ba13b2e19e32b0
Akinkunmi <akinkunmiisrael41@gmail.com>
2022-09-24 06:31:44 +0100

    .

---
+++ pages/dashboard/heading.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import DashboardLayout from "../../layouts/DashboardLayout";
+
+const Heading = () => {
+  return <DashboardLayout>Heading</DashboardLayout>;
+};
+
+export default Heading;
