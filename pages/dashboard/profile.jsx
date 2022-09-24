a6c57d407bc71fb15624604b4b1c14ed39c33c32
Akinkunmi <akinkunmiisrael41@gmail.com>
2022-09-24 06:31:50 +0100

    .

--- pages/dashboard/profile.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import DashboardLayout from "../../layouts/DashboardLayout";
-
-const Profile = () => {
-  return <DashboardLayout>Profile</DashboardLayout>;
-};
-
-export default Profile;
