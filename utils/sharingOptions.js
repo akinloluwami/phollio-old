5f493325a83c788b2279adba3448b7d5dee6c1e6
Akinkunmi <akinkunmiisrael41@gmail.com>
2022-09-25 23:15:09 +0100

    sharing options...

---
+++ utils/sharingOptions.js
@@ -0,0 +1,13 @@
+const sharingOptions = [
+  {
+    title: "Add to your social",
+  },
+  {
+    title: "Share my Phollio to...",
+  },
+  {
+    title: "My Phollio QR code",
+  },
+];
+
+export default sharingOptions;
