063d6879b4134d2d843102069c2109df78136c8a
Akinkunmi <akinkunmiisrael41@gmail.com>
2022-09-25 23:14:54 +0100

    added input fields and upload button

--- pages/dashboard/heading.jsx
+++ pages/dashboard/heading.jsx
@@ -1,8 +1,28 @@
+import { Avatar, Box, Button, Flex, Input, Textarea } from "@chakra-ui/react";
 import React from "react";
 import DashboardLayout from "../../layouts/DashboardLayout";
 
 const Heading = () => {
-  return <DashboardLayout>Heading</DashboardLayout>;
+  return (
+    <DashboardLayout>
+      <Flex flexDirection={"column"} alignItems={"center"}>
+        <Flex alignItems={"center"} my={5}>
+          <Avatar size={"xl"} name={"Akinkunmi"} />
+          <Button size={"sm"} ml={5}>
+            Add profile image
+          </Button>
+        </Flex>
+        <Box width={"70%"}>
+          <Input placeholder={"Display name"} my={2} />
+          <Input placeholder={"Professional title"} my={2} />
+          <Textarea placeholder={"Bio"} my={2} />
+        </Box>
+        <Button my={2} backgroundColor={"brand.accent"}>
+          Sace changes
+        </Button>
+      </Flex>
+    </DashboardLayout>
+  );
 };
 
 export default Heading;
