a804481215e1ad3472cb15867f115278ce4989ef
Akinkunmi <akinkunmiisrael41@gmail.com>
2022-09-26 23:45:11 +0100

    set up page structure

--- pages/dashboard/projects.jsx
+++ pages/dashboard/projects.jsx
@@ -1,8 +1,15 @@
+import { Box, Flex } from "@chakra-ui/react";
 import React from "react";
 import DashboardLayout from "../../layouts/DashboardLayout";
 
 const Projects = () => {
-  return <DashboardLayout>Projects</DashboardLayout>;
+  return (
+    <DashboardLayout>
+      <Box>
+        <Flex></Flex>
+      </Box>
+    </DashboardLayout>
+  );
 };
 
 export default Projects;
