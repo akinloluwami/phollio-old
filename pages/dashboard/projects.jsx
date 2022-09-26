3a55ab2b4943bbb338ceec81fada0a8be292be8e
Akinkunmi <akinkunmiisrael41@gmail.com>
2022-09-26 23:55:02 +0100

    imported project component

--- pages/dashboard/projects.jsx
+++ pages/dashboard/projects.jsx
@@ -1,12 +1,18 @@
 import { Box, Flex } from "@chakra-ui/react";
 import React from "react";
+import Project from "../../components/dashboard/Project";
 import DashboardLayout from "../../layouts/DashboardLayout";
 
 const Projects = () => {
   return (
     <DashboardLayout>
       <Box>
-        <Flex></Flex>
+        <Flex flexWrap={"wrap"} alignItems={"center"} justifyContent={"center"}>
+          <Project />
+          <Project />
+          <Project />
+          <Project />
+        </Flex>
       </Box>
     </DashboardLayout>
   );
