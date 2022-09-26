a0cdb72e2f0e48e702784266f12fc4f19097500a
Akinkunmi <akinkunmiisrael41@gmail.com>
2022-09-26 23:54:39 +0100

    setup basic styling...

--- components/dashboard/Project.jsx
+++ components/dashboard/Project.jsx
@@ -1,7 +1,20 @@
+import { Box, Input } from "@chakra-ui/react";
 import React from "react";
 
 const Project = () => {
-  return <div>Project</div>;
+  return (
+    <Box width={"250px"} my={3} mx={5}>
+      <Box
+        width={"250px"}
+        height={"150px"}
+        bg={"rgba(0,0,0,0.2)"}
+        mb={2}
+        borderRadius={"md"}
+      ></Box>
+      <Input placeholder="Title" mb={2} />
+      <Input placeholder="URL" />
+    </Box>
+  );
 };
 
 export default Project;
